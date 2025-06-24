af74a15e7069014a3370d037fb6f061dc572c0e1
MarkDuek <duekmark@gmail.com>
2025-06-24 20:17:15 -0300

    feat: updated AmazonDataset to slice npz files and only convert to dense matrix on get_item

--- src/data/amazon_dataset.py
+++ src/data/amazon_dataset.py
@@ -1,3 +1,4 @@
+import l"""  """ogging
 import numpy as np
 import torch
 from torch.utils.data import Dataset
@@ -6,29 +7,37 @@ from typing import Any, Mapping, List
 
 class AmazonDataset(Dataset):
 
-    def __init__(self, data_paths, transform=None):
+    def __init__(self, data_paths, time_slice, transform=None):
+        self.logger = logging.getLogger(__name__)
+        paths_str = '\n'.join([f"  - {path}" for path in data_paths])
+        self.logger.info(f"Initializing AmazonDataset with data paths:\n{paths_str}")
         self.data_paths = data_paths
         self.transform = transform
-        self.data = self.load_data(data_paths)
-        print(self.data.shape)
+        self.data = self.load_npz_files(data_paths)
+        self.time_slice = time_slice
+        self.total_time_steps = len(self.data[0].keys())
 
     def __len__(self):
-        return len(self.data)
+        return self.total_time_steps - self.time_slice + 1
 
-    def __getitem__(self, idx):
-        # TODO: get time slice from self.data based on a specific timeframe and convert to tensor
-
-        return torch.tensor()
-
-    def load_data(
-        self,
-        data_paths: List[str],
-    ) -> np.ndarray:
-
-        data_list = self.load_npz_files(data_paths)
-        data = self.concatenate_sparse_matrix(data_list)
-
-        return data
+    def __getitem__(self, idx: int):
+        self.logger.info(f"Getting item at index: {idx}")
+        time_slice_data = self.get_time_slice(idx, self.time_slice)
+        time_slice_data = self.concatenate_sparse_matrix(time_slice_data)
+        
+        return torch.tensor(time_slice_data)
+    
+    def get_time_slice(self, start_idx: int, time_slice: int):
+        file_channels = []
+        
+        for file_data in self.data:
+            subset_data = {}
+            for i, time_step in enumerate(range(start_idx, start_idx + time_slice)):
+                subset_data[f"arr_{i}"] = file_data[f"arr_{time_step}"]
+            
+            file_channels.append(subset_data)
+        
+        return file_channels
 
     def load_npz_files(
         self,
@@ -53,7 +62,6 @@ class AmazonDataset(Dataset):
 
         array_3d: np.ndarray = np.stack(frames).astype(d_type)
 
-        print(array_3d.shape)
         return array_3d
 
     def concatenate_sparse_matrix(
@@ -65,4 +73,4 @@ class AmazonDataset(Dataset):
             self.stack_sparse_matrix(data, d_type=np.float32) for data in data_list
         ]
 
-        return np.concatenate(arrays)
+        return np.stack(arrays)
