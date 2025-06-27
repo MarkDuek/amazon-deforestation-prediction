406de85c8a9e3bc7278c2e8c9ba22ca4492b9ddf
MarkDuek <duekmark@gmail.com>
2025-06-26 21:10:38 -0300

    tests: added tests for deep_lab_v3

---
+++ tests/models/deep_lab_v3_test.py
@@ -0,0 +1,9 @@
+import pytest
+import torch
+
+from src.models.deep_lab_v3.model import DeepLabV3
+
+def test_forward(config, input_tensor):
+    model = DeepLabV3(config)
+    output = model.forward(input_tensor)
+    assert output.shape == (input_tensor.shape[0], config["model"]["deep_lab_v3"]["classes"], 1, input_tensor.shape[3], input_tensor.shape[4]) # (B, 1, 1, H, W)
