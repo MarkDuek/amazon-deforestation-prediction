5950b916463f5ff9ebf3d597299c2bf7ff28cb09
MarkDuek <duekmark@gmail.com>
2025-06-27 11:12:07 -0300

    feat: updated main to pass config to Amazondataset and defined model, optimizer, loss_fn and trainer to call training loop

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from torch.utils.data import random_split, DataLoader
 
 from src.data.amazon_dataset import AmazonDataset
 from src.utils.utils import get_device, parse_args, load_config
+from src.models.deep_lab_v3.model import DeepLabV3
+from src.training.trainer import Trainer
 
 
 def main():
@@ -18,40 +20,40 @@ def main():
     # get config variables
     device = get_device(config["device"])
     val_ratio = config["data"]["val_ratio"]
-    paths = config["data"]["paths"]
-    time_slice = config["data"]["time_slice"]
 
     # load dataset
-    amazon_dataset = AmazonDataset(paths, time_slice)
-    logger.info(f"Dataset sample shape: {amazon_dataset[0].shape}")
+    amazon_dataset = AmazonDataset(config)
+    logger.info(f"Dataset sample shape: {amazon_dataset[0][0].shape}")
 
     # split dataset
     train_data, val_data = random_split(amazon_dataset, [(1 - val_ratio), val_ratio])
 
     # create dataloaders
+    logger.info(f"Creating train loader")
     train_loader = DataLoader(
         train_data,
         batch_size=config["training"]["batch_size"],
         shuffle=True,
-        num_workers=4,
     )
+    logger.info(f"Creating val loader")
     val_loader = DataLoader(
         val_data,
         batch_size=config["training"]["batch_size"],
         shuffle=False,
-        num_workers=4,
     )
 
-    # TODO: define model
-    # model = Model()
+    model = DeepLabV3(config)
+    model.to(device)
 
-    # optimizer = torch.optim.Adam(model.parameters(), lr=config["training"]["learning_rate"])
+    optimizer = torch.optim.Adam(model.parameters(), lr=config["training"]["learning_rate"])
+    logger.info(f"Optimizer summary: {optimizer}")
 
-    # TODO: define loss function
-    # loss_fn = 
+    loss_fn = torch.nn.BCEWithLogitsLoss()
+    logger.info(f"Loss function summary: {loss_fn}")
 
-    # trainer = Trainer(model, train_loader, val_loader, optimizer, loss_fn, device)
-    # trainer.train()
+    trainer = Trainer(model, train_loader, val_loader, optimizer, loss_fn, device, config)
+    logger.info(f"Starting training...")
+    trainer.train()
 
 if __name__ == "__main__":
     main()
